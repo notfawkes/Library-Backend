1ba29b430e7ebc8b11312c11860e6c566a505bf5
Bala Sudalaimuthu <bala@Balas-MacBook-Air.local>
2025-10-26 22:24:46 +0530

    Allow credentials

--- src/main/java/com/library/config/WebConfig.java
+++ src/main/java/com/library/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig {
                         .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowCredentials(false);
             }
         };
     }
